d6877b88a396a76e35859f1ccca285d779fa5e70
shifantasticc <shifastudyit@gmail.com>
2026-07-29 21:29:38 +0530

    Adding new component: Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>SKY FOR ALL</h2>
+
+      <p>Made with ❤️ for astronomy lovers.</p>
+
+      <div>
+        <button>Discord</button>
+        <button>Instagram</button>
+        <button>GitHub</button>
+      </div>
+
+      <p>©2026 Sky For All</p>
+    </footer>
+  );
+}
+
+export default Footer;
