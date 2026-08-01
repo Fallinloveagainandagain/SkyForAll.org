66b54af6aae2d72b176b36ed410dbadc1040f1bc
shifantasticc <shifastudyit@gmail.com>
2026-08-01 18:05:56 +0530

    Updating Files: App.jsx, App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import Hero from "./components/Hero/Hero";
+import './App.css';
+
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Quiz from './components/Quiz/Quiz';
+import Volunteer from './components/Volunteer/Volunteer';
+import Resources from './components/Resources/Resources';
+import Roadmap from './components/Roadmap/Roadmap';
+import SkyPlanner from './components/SkyPlanner/SkyPlanner';
+import Footer from './components/Footer/Footer';
+
+import Stars from './components/Stars/Stars';
+import Planet from './components/Planet/Planet';
 
 function App() {
   return (
-    <>
-      <Hero />
-    </>
+    <div className="app">
+      <div className="background"></div>
+
+      <Stars />
+
+      <Planet />
+
+      <div className="content">
+        <Hero />
+
+        <section className="two-column">
+          <About />
+          <Quiz />
+        </section>
+
+        <section className="two-column">
+          <Volunteer />
+          <Resources />
+        </section>
+
+        <section className="roadmap-section">
+          <Roadmap />
+        </section>
+
+        <section className="planner-section">
+          <SkyPlanner />
+        </section>
+
+        <Footer />
+      </div>
+    </div>
   );
 }
 
