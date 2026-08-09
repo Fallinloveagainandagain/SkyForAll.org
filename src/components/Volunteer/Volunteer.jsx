aad6741b255bad89865279f1dbcf6711e216b360
shifantasticc <shifastudyit@gmail.com>
2026-08-10 00:51:02 +0530

    Updating Files: Volunteer.jsx, Volunteer.css

--- src/components/Volunteer/Volunteer.jsx
+++ src/components/Volunteer/Volunteer.jsx
@@ -5,14 +5,26 @@ function Volunteer() {
   return (
     <RetroWindow title="volunteer_portal.exe">
       <div className="volunteer">
-        <h2>JOIN THE CREW</h2>
+        <h2>
+          <i className="fa-regular fa-star"></i> JOIN THE CREW
+        </h2>
 
         <p>
-          Help organize astronomy events, create educational content, and
-          inspire future explorers.
+          Help us run digital events, post on social media, and write about
+          space from your bedroom. Zero experience needed!
         </p>
 
-        <button className="join-btn">APPLY NOW</button>
+        <div className="form">
+          <i className="fa-regular fa-file-lines"> Intake form 2026</i>
+          <a
+            className="join-btn"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSclBmLCKfMOW7qF7y5G4ZferS8rV7U0R3dSMUfbfovGNuhRnA/viewform"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            APPLY NOW
+          </a>
+        </div>
       </div>
     </RetroWindow>
   );
