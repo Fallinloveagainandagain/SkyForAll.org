6128bd76e6771fffe2408be4f9ed2175e3c12dda
shifantasticc <shifastudyit@gmail.com>
2026-08-16 02:11:56 +0530

    Updating Files: Resources.jsx, Resources.css

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,61 +1,55 @@
-import "./Resources.css";
-import RetroWindow from "../RetroWindow/RetroWindow";
+import './Resources.css';
+import RetroWindow from '../RetroWindow/RetroWindow';
 
 function Resources() {
   const resources = [
     {
-      title: "Stellarium Web",
-      description: "Explore the night sky in your browser.",
-      icon: "fa-solid fa-earth-americas",
-      link: "https://stellarium-web.org/",
+      title: 'Stellarium Web',
+      description: 'Explore the night sky in your browser.',
+      icon: 'fa-solid fa-earth-americas',
+      link: 'https://stellarium-web.org/',
     },
     {
-      title: "NASA APOD",
+      title: 'NASA APOD',
       description: "NASA's Astronomy Picture of the Day.",
-      icon: "fa-solid fa-camera-retro",
-      link: "https://apod.nasa.gov/apod/",
+      icon: 'fa-solid fa-camera-retro',
+      link: 'https://apod.nasa.gov/apod/',
     },
     {
-      title: "Astronomy Basics",
-      description: "Learn the fundamentals of astronomy.",
-      icon: "fa-solid fa-book-open",
-      link: "https://science.nasa.gov/universe/",
+      title: 'Astronomy Basics',
+      description: 'Learn the fundamentals of astronomy.',
+      icon: 'fa-solid fa-book-open',
+      link: 'https://science.nasa.gov/universe/',
     },
   ];
 
   return (
     <RetroWindow title="learning_archives.txt">
       <div className="resources">
-
-        <h2>
+        <div className="title">
           <i className="fa-solid fa-book-open"></i> LEARNING RESOURCES
-        </h2>
+          <hr />
+        </div>
 
         <div className="resource-grid">
-
           {resources.map((resource, index) => (
             <div className="resource-card" key={index}>
-
-              <i className={`${resource.icon} resource-icon`}></i>
-
-              <h3>{resource.title}</h3>
-
-              <p>{resource.description}</p>
-
               <a
                 href={resource.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="resource-btn"
               >
-                Open Resource
+                <i className={`${resource.icon} resource-icon`}></i>
+                <h3>
+                  <div className="resource-title">{resource.title}</div>
+                  <p className="resource-description">{resource.description}</p>
+                </h3>
+                <i className="link fa-solid fa-arrow-up-right-from-square"></i>
               </a>
-
             </div>
           ))}
-
         </div>
-
       </div>
     </RetroWindow>
   );
