8de89a43eda254ec0a07f21ab649cbdf9bbd243d
shifantasticc <shifastudyit@gmail.com>
2026-08-18 01:54:03 +0530

    Updating Files: Volunteer.jsx, Volunteer.css

--- src/components/Volunteer/Volunteer.jsx
+++ src/components/Volunteer/Volunteer.jsx
@@ -5,9 +5,9 @@ function Volunteer() {
   return (
     <RetroWindow title="volunteer_portal.exe">
       <div className="volunteer">
-        <h2>
+        <div className="title">
           <i className="fa-regular fa-star"></i> JOIN THE CREW
-        </h2>
+        </div>
 
         <p>
           Help us run digital events, post on social media, and write about
@@ -15,15 +15,19 @@ function Volunteer() {
         </p>
 
         <div className="form">
-          <i className="fa-regular fa-file-lines"> Intake form 2026</i>
-          <a
-            className="join-btn"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSclBmLCKfMOW7qF7y5G4ZferS8rV7U0R3dSMUfbfovGNuhRnA/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            APPLY NOW
-          </a>
+          <div className="form-title">
+            <i className="fa-regular fa-file-lines"></i> Intake form 2026
+          </div>
+          <div className="form-description">
+            <a
+              className="join-btn"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSclBmLCKfMOW7qF7y5G4ZferS8rV7U0R3dSMUfbfovGNuhRnA/viewform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              APPLY NOW
+            </a>
+          </div>
         </div>
       </div>
     </RetroWindow>
